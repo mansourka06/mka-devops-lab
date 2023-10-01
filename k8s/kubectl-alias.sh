6b79a4f624f08dc630bb6e393fe2d84afaff5f3d
Mansour KA <kamansour06@gmail.com>
2023-10-01 15:09:49 +0200

    Update files

--- k8s/kubectl-alias.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-alias k='kubectl' && alias kcc='kubectl config current-context'
-alias kg='kubectl get' && alias ka='kubectl apply'
-alias kc='kubectl create' && alias kgp='kubectl get pods' && alias kga='kubectl get all'
